a84ed2bed64921cd0351be9abe21b95cb5d16f92
ichi-09ryo <5t6w.jl@gmail.com>
2024-05-10 17:33:52 +0900

    commentsコントローラーのprivateメソッドにストロングパラメーターセット

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
   def create
   end
+
+  private
+   def comment_params
+    params.require(:comment).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
 end
